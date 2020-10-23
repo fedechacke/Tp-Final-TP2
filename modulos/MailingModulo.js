0351d794447456105e7fc63d212cf59f5afda725
Tomás Lozano <tomas.lozano92@gmail.com>
2020-10-22 22:07:49 -0300

    Modulo Mailing actualizado a modulo

--- modulos/MailingModulo.js
+++ modulos/MailingModulo.js
@@ -1,54 +1,51 @@
 const nodemailer = require('nodemailer')
 
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'gestion.remiseria@gmail.com',
-            pass: 'TPgrupal2-'
-        },
-    })
-
-    function setEmailService (service, user, pass){
-        transporter.service = service
-        transporter.auth.user = user
-        transporter.auth.pass = pass
-    }    
-
-    const mailData = {
-        from: 'from',
-        to: 'to',
-        subject: 'emptySubject',
-        text: 'text'
-    } 
+function crearModuloMailing(mailService, username, password) {
     
-    /** 
-    *@param {string} from  own Mail address
-    *@param {string} to: the receptor of your mail
-    *@param {string} subject: title of your mail
-    *@param {string} text: body/content
-    */
-    function completeMailData (from, to, subject, text) {
-        mailData.from=from
-        mailData.to=to
-        mailData.subject=subject
-        mailData.text=text
-    }
-
-    function validateMailContent(){
+    const validateMailFormat = function (mailData) {
         const emailRejex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-        return (emailRejex.test(mailData.from) && emailRejex.test(mailData.to))
+        return (emailRejex.test(mailData))
+    }
+
+    let transporter
+    if (validateMailFormat(username)){
+        transporter = nodemailer.createTransport({
+            service: mailService,
+            auth: {
+                user: username,
+                pass: password
+            }
+        })
+    } else {
+        throw new Error('Usuario invalido');
     }
     
-    function enviarMail () {
-        if (validateMailContent()){
-            transporter.sendMail(mailData);
-        } else {
-            throw new Error()
+    return {
+        /** 
+        *@param {string} to: the recipients of your mail
+        *@param {string} subject: subject of your mail
+        *@param {string} mailBody: body/content
+        */ 
+        enviarMail: async function (to, subject, mailBody) {
+           
+           const mailData = {
+               from: this.username,
+               to: to,
+               subject: subject,
+               text: mailBody
+            } 
+            
+            await transporter.sendMail(mailData, (err, info) => {
+                if (err){
+                    console.log(`Error: ${err.message}`);
+                } else {
+                    console.log(`Mail enviado con exito a: ${info.accepted}`)
+                }
+            })
         }
-
     }
+}
 
-module.exports = { completeMailData, enviarMail, setEmailService }
+module.exports = { crearModuloMailing }
 
 

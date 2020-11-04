30014d9642102dac26e9c3038f93a03c61ba8e97
Tomás Lozano <tomas.lozano92@gmail.com>
2020-11-04 10:34:06 -0300

    Refactorizado el CU1

--- CU/CU.test.js
+++ CU/CU.test.js
@@ -1,7 +1,12 @@
+const { crearModuloMailing } = require('../modulos/MailingModulo/MailingModulo.js');
+const { crearTemporizadorDeEventos } = require('../modulos/ModuloTemporizador/ScheduleModulo.js');
 const { crearCu } = require('./CU.js');
 
+const moduloMail = crearModuloMailing('gmail', 'gestion.remiseria@gmail.com', 'TPgrupal2-');
+const moduloSchedule = crearTemporizadorDeEventos();
+
 function main () {
-    const cu = crearCu();
+    const cu = crearCu(moduloMail, moduloSchedule);
     cu.invocar();
 
 }
